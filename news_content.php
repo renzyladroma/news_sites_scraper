5acf0ab60bd5fc5fd117829ad42b428a9d5c0f61
renzyladroma <32568765+renzyladroma@users.noreply.github.com>
2017-11-27 13:40:20 +0800

    Update news_content.php

--- news_content.php
+++ news_content.php
@@ -1,8 +1,8 @@
 <?php
 header('Content-type: text/json; charset=utf-8');
 error_reporting(0);
-$connection = mysqli_connect("120.28.24.42","root","jfr3u9t","abaka_db");
-//$con = mysqli_connect("localhost","root","","ctifls_db");
+$connection = mysqli_connect("","","","");
+
 mysqli_set_charset($connection,"utf8");
 
 //Check connection
