bb4d3576ebdc6c28d15673137adc751b8130c921
renzyladroma <32568765+renzyladroma@users.noreply.github.com>
2017-11-27 13:39:25 +0800

    Update recommended.php

--- recommended.php
+++ recommended.php
@@ -1,8 +1,8 @@
 <?php
 header('Content-type: text/json');
 error_reporting(0);
-$connection = mysqli_connect("120.28.24.42","root","jfr3u9t","abaka_db");
-//$con = mysqli_connect("localhost","root","","ctifls_db");
+$connection = mysqli_connect("","","","");
+
 
 //Check connection
 if (mysqli_connect_errno()) {
